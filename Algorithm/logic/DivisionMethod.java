e10c30ecf592541b0a833575283234a40e095f3f
tetsu <root2120@gmail.com>
2024-02-03 08:15:01 +0900

    アルゴリズムの練習

--- Algorithm/logic/DivisionMethod.java
+++ Algorithm/logic/DivisionMethod.java
@@ -80,16 +80,8 @@ public class DivisionMethod {
 	//戻り値を返すタイプ
 	public int exec2( int x , int y ) {
 		
-		//両方が同じ数値の場合は、処理終了
-		if( x == y ) {
-			return 1 ;
-		}
-		
-		if( x == 1 || y == 1) {
-			return 1;
-		}
-		
 		//メインロジック
+		//大きい数値を小さい数値で割った余りを求める。
 		while( x >= 1 && y >= 1 ) {
 			if( x > y ){
 				x = x % y;
@@ -103,9 +95,32 @@ public class DivisionMethod {
 		return bigNumber;
 	}
 	
+	//リファクタリング 再帰関数による繰り返し処理
+	//xが割られる数、yが割る数
+	public int exec4( int x , int y ) {
+		
+		if(y == 0){
+			return x;
+		}
+		
+		//xとyの大きさ関係なく、xをyで割った後の値の方が小さくなる。
+		//そのため、xとyの数値の大小比較が冗長となる。
+		//前者の引数が大きい数値、後者が小さい数値⇒再帰される際は、変数が入れ替わることに注意
+		//「%」は割る方の数値yの方が割られる数xよりも大きい場合、xをそのまま返す。
+		return exec4( y , x % y); 
+		
+	}
+	
+//	public void exec3( int x , int y ) {
+//		
+//		System.out.println( x % y );
+//		
+//	}
+
+	
 	//複数の数の最大公約数
 	public void exec( List<Integer> numberList ){
-		int result = numberList.stream().reduce(0,(x,y) -> this.exec2(x, y) );
+		int result = numberList.stream().reduce(0,(x,y) -> this.exec2(x, y));
 		System.out.println( result );
 	}
 }
